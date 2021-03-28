a9a16054d24a7cd54ea336db9691c3b20e48e506
Nick Doyle <njdoyle@gmail.com>
2021-03-28 18:09:08 -0400

    Remove old OLED code.

--- users/njdoyle/config.h
+++ users/njdoyle/config.h
@@ -1,11 +1,11 @@
 #pragma once
 
-// GENERAL
+/* GENERAL */
 #define FORCE_NKRO
-#define ONESHOT_TAP_TOGGLE 0
-#define TAPPING_TERM 200
+#define ONESHOT_TAP_TOGGLE   0
+#define TAPPING_TERM       200
 
-// MOUSE
+/* MOUSE */
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
 #define MOUSEKEY_DELAY      100
@@ -20,20 +20,16 @@
 #define MK_C_OFFSET_2        32
 #define MK_C_INTERVAL_2      16
 
+/* OLED */
 #ifdef OLED_DRIVER_ENABLE
-#	define NJD_OLED_NEW
-#	ifdef NJD_OLED_NEW
-#		define OLED_FONT_WIDTH  8
-#		define OLED_FONT_HEIGHT 8
-#		define OLED_FONT_START  0x00
-#		define OLED_FONT_END    0x8F
-#		define OLED_FONT_H "users/njdoyle/njd_oled_font.c"
-#	else
-#		define OLED_FONT_H "drivers/oled/glcdfont.c"
-#	endif
+#	define OLED_FONT_WIDTH  8
+#	define OLED_FONT_HEIGHT 8
+#	define OLED_FONT_START  0x00
+#	define OLED_FONT_END    0x8F
+#	define OLED_FONT_H      "users/njdoyle/njd_oled_font.c"
 #endif
 
-// AUDIO
+/* AUDIO */
 #ifdef AUDIO_ENABLE
 #	define DEFAULT_LAYER_SONGS { \
 		SONG(QWERTY_SOUND ), \
--- users/njdoyle/njd_oled.c
+++ users/njdoyle/njd_oled.c
@@ -2,82 +2,6 @@
 
 #ifdef OLED_DRIVER_ENABLE
 
-#ifndef NJD_OLED_NEW
-
-static void njd_oled_render_layout_state(void) {
-	oled_write_P(PSTR("Layout: "), false);
-	switch (get_highest_layer(default_layer_state)) {
-		case NJD_L_QWERTY     : oled_write_ln_P(PSTR("QWERTY"     ), false); break;
-		case NJD_L_DVORAK     : oled_write_ln_P(PSTR("Dvorak"     ), false); break;
-		case NJD_L_COLEMAK    : oled_write_ln_P(PSTR("Colemak"    ), false); break;
-		case NJD_L_COLEMAK_DH : oled_write_ln_P(PSTR("Colemak DH" ), false); break;
-		case NJD_L_COLEMAK_DHK: oled_write_ln_P(PSTR("Colemak DHk"), false); break;
-		case NJD_L_WORKMAN    : oled_write_ln_P(PSTR("Workman"    ), false); break;
-		default               : oled_write_ln_P(PSTR("UNKNOWN"    ), false); break;
-	}
-}
-
-static void njd_oled_render_os_mode(void) {
-	oled_write_P(PSTR("  Mode: "), false);
-	switch (njd_get_os_mode()) {
-		case NJD_OS_MACOS  : oled_write_ln_P(PSTR("macOS"  ), false); break;
-		case NJD_OS_LINUX  : oled_write_ln_P(PSTR("Linux"  ), false); break;
-		case NJD_OS_WINDOWS: oled_write_ln_P(PSTR("Windows"), false); break;
-		default            : oled_write_ln_P(PSTR("UNKNOWN"), false); break;
-	}
-}
-
-static void njd_oled_render_layer_state(void) {
-	oled_write_P(PSTR(" Layer: "), false);
-	switch (get_highest_layer(layer_state)) {
-		case 0               : oled_write_ln_P(PSTR("Base"      ), false); break;
-		case NJD_L_LOWER     : oled_write_ln_P(PSTR("Lower"     ), false); break;
-		case NJD_L_RAISE     : oled_write_ln_P(PSTR("Raise"     ), false); break;
-		case NJD_L_NAVIGATION: oled_write_ln_P(PSTR("Navigation"), false); break;
-		case NJD_L_MOUSE     : oled_write_ln_P(PSTR("Mouse"     ), false); break;
-		case NJD_L_FUNCTION  : oled_write_ln_P(PSTR("Function"  ), false); break;
-		case NJD_L_OPTIONS   : oled_write_ln_P(PSTR("Options"   ), false); break;
-		default              : oled_write_ln_P(PSTR("UNKNOWN"   ), false); break;
-	}
-}
-
-static void njd_oled_render_host_led_state(void) {
-	uint8_t leds = host_keyboard_leds();
-	oled_write_P   (PSTR(" Locks: "), false);
-	oled_write_P   ((leds & (1 << USB_LED_CAPS_LOCK  )) ? PSTR("C") : PSTR("-"), false);
-	oled_write_P   ((leds & (1 << USB_LED_NUM_LOCK   )) ? PSTR("N") : PSTR("-"), false);
-	oled_write_ln_P((leds & (1 << USB_LED_SCROLL_LOCK)) ? PSTR("S") : PSTR("-"), false);
-}
-
-static void njd_oled_render_logo(void) {
-	static const char PROGMEM logo[] = {
-		0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8A,0x8B,0x8C,0x8D,0x8E,0x8F,0x90,0x91,0x92,0x93,0x94,
-		0xA0,0xA1,0xA2,0xA3,0xA4,0xA5,0xA6,0xA7,0xA8,0xA9,0xAA,0xAB,0xAC,0xAD,0xAE,0xAF,0xB0,0xB1,0xB2,0xB3,0xB4,
-		0xC0,0xC1,0xC2,0xC3,0xC4,0xC5,0xC6,0xC7,0xC8,0xC9,0xCA,0xCB,0xCC,0xCD,0xCE,0xCF,0xD0,0xD1,0xD2,0xD3,0xD4,
-		0};
-	oled_write_P(logo, false);
-}
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-	return is_master ? rotation : OLED_ROTATION_180;
-}
-
-void oled_task_user(void) {
-	if (is_master) {
-		njd_oled_render_layout_state  ();
-		njd_oled_render_os_mode       ();
-		njd_oled_render_layer_state   ();
-		njd_oled_render_host_led_state();
-	} else {
-		njd_oled_render_logo();
-	}
-}
-
-#else
-
-
-/* SPRITES */
-
 static const char PROGMEM njd_sprite_logo[4*16] = {
 	0x20,0x36,0x3A,0x3A,0x3A,0x20,0x30,0x3A,0x3A,0x3C,0x20,0x3A,0x3E,0x3A,0x3C,0x20,
 	0x20,0x35,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x35,0x20,0x20,0x35,0x20,0x35,0x20,
@@ -117,8 +41,6 @@ static const char PROGMEM njd_sprite_locks[2*6] = {
 	0x80,0x8C, 0x13,0x19, 0x13,0x18
 };
 
-
-/* LOGIC */
 
 void njd_oled_write_chars_P(const char* data, uint8_t count) {
 	uint8_t c;
@@ -185,7 +107,5 @@ void oled_task_user(void) {
 		njd_oled_write_chars_P(njd_sprite_logo, 4 * 16);
 	}
 }
-
-#endif /* ndef NJD_OLED_NEW */
 
 #endif /* def OLED_DRIVER_ENABLE */
